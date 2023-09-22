bfe1ca882fd2e64bed487642819b396f4be2f330
AnjaniPolsani <137593183+AnjaniPolsani@users.noreply.github.com>
2023-09-22 16:29:14 +0530

    Update index.js

--- src/components/MatchGame/index.js
+++ src/components/MatchGame/index.js
@@ -6,7 +6,6 @@ const MatchGame = props => {
   return (
     <nav >
       <img
-          
           src="https://assets.ccbp.in/frontend/react-js/match-game-website-logo.png"
           alt="website logo"
         />
@@ -15,9 +14,7 @@ const MatchGame = props => {
         <p >Score: <span>{score}</span></p>
       </li>
       <li>
-        
-          <img
-            
+        <img
             src="https://assets.ccbp.in/frontend/react-js/match-game-timer-img.png"
             alt="timer"
           />
